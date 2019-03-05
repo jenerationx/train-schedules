accf3d7ad84035d157c56176c4a26f7738c3cfcd
jenerationx <jenerationx74@gmail.com>
2019-03-04 21:59:25 -0500

    html & css done

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDRkIus8bN3DahjVRAgL6MsBO5x1rK2_Pk",
+    authDomain: "train-time-fb995.firebaseapp.com",
+    databaseURL: "https://train-time-fb995.firebaseio.com",
+    projectId: "train-time-fb995",
+    storageBucket: "train-time-fb995.appspot.com",
+    messagingSenderId: "99164360758"
+  };
+  firebase.initializeApp(config);
